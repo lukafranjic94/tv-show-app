3421348103b8558cf20f61361e494332de98ab82
Luka Franjić <lukafranjic94@gmail.com>
2021-07-06 23:07:39 +0200

    Add delete review functionality

--- work/index.js
+++ work/index.js
@@ -5,32 +5,32 @@ class Review {
 	}
 }
 
-
-const storedReviews = [new Review('bongland humor lol', 3), new Review('not my cup of tea', 1), new Review('amazing', 5)];
-localStorage.setItem('storedReviews', JSON.stringify(storedReviews))
-updateAverageRating()
-
-const form = document.querySelector('#review-form');
-form.addEventListener('submit', (event) => {
-	event.preventDefault();
-	const storedReviews = JSON.parse(localStorage.getItem('storedReviews'));
-	const reviewBody = document.querySelector('#review-body').value;
-	const reviewRating = document.querySelector('#review-rating').value;
+function createReviewElement(body, rating) {
 	const newReview = document.createElement('div');
 	const newReviewBody = document.createElement('p');
 	const newReviewRating = document.createElement('p');
-	const reviews = document.querySelector('#reviews')
+	const newReviewDeleteButton = document.createElement('button')
 
-	newReviewBody.textContent = reviewBody
-	newReviewRating.textContent = `${reviewRating}/5`
-	newReview.classList.add("review")
+	newReviewDeleteButton.id = '#delete-button'
+	newReviewDeleteButton.textContent = 'Remove'
+	newReviewDeleteButton.addEventListener('click', (event) => {
+		const parentReview = event.currentTarget.parentNode;
+		const reviews = parentReview.parentNode;
+		const index = Array.from(reviews.children).indexOf(parentReview);
+		const storedReviews = JSON.parse(localStorage.getItem('storedReviews'));
+		storedReviews.splice(index, 1);
+		localStorage.setItem('storedReviews', JSON.stringify(storedReviews));
+		updateAverageRating()
+		reviews.removeChild(parentReview);
+	})
+	newReviewBody.textContent = body
+	newReviewRating.textContent = `${rating}/5`
+	newReview.classList.add('review')
 	newReview.appendChild(newReviewBody);
 	newReview.appendChild(newReviewRating);
-	reviews.appendChild(newReview);
-	storedReviews.push(new Review(reviewBody, parseInt(reviewRating)));
-	localStorage.setItem('storedReviews', JSON.stringify(storedReviews));
-	updateAverageRating()
-});
+	newReview.appendChild(newReviewDeleteButton);
+	return newReview;
+}
 
 function updateAverageRating() {
 	const storedReviews = JSON.parse(localStorage.getItem('storedReviews'));
@@ -40,20 +40,31 @@ function updateAverageRating() {
 }
 
 function initialize() {
+	const reviewArray = [new Review('bongland humor lol', 3), new Review('not my cup of tea', 1), new Review('amazing', 5)];
+	localStorage.setItem('storedReviews', JSON.stringify(reviewArray))
+	updateAverageRating()
+
 	const reviews = document.querySelector('#reviews')
 	const storedReviews = JSON.parse(localStorage.getItem('storedReviews'));
 	storedReviews.forEach(review => {
-		const newReview = document.createElement('div');
-		const newReviewBody = document.createElement('p');
-		const newReviewRating = document.createElement('p');
-
-		newReviewBody.textContent = review.body;
-		newReviewRating.textContent = `${review.rating}/5`
-		newReview.classList.add("review")
-		newReview.appendChild(newReviewBody);
-		newReview.appendChild(newReviewRating);
+		const newReview = createReviewElement(review.body, review.rating)
 		reviews.appendChild(newReview);
 	})
+
+	const form = document.querySelector('#review-form');
+	form.addEventListener('submit', (event) => {
+		event.preventDefault();
+		const storedReviews = JSON.parse(localStorage.getItem('storedReviews'));
+		const reviewBody = document.querySelector('#review-body').value;
+		const reviewRating = document.querySelector('#review-rating').value;
+		const newReview = createReviewElement(reviewBody, reviewRating)
+
+		reviews.appendChild(newReview);
+		storedReviews.push(new Review(reviewBody, parseInt(reviewRating)));
+		localStorage.setItem('storedReviews', JSON.stringify(storedReviews));
+		updateAverageRating()
+	});
+
 }
 
 initialize();
