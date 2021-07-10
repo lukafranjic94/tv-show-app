151d7fdfc9c0b6670aa88bb32e6b3b44d0f0c121
Luka Franjić <lukafranjic94@gmail.com>
2021-07-10 19:17:47 +0200

    Add MainLayoutComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { ShowCardComponent } from './components/show-card/show-card.component';
 import { ShowListComponent } from './components/show-list/show-list.component';
 import { AllShowsContainerComponent } from './pages/all-shows-container/all-shows-container.component';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
 	declarations: [
@@ -18,8 +20,9 @@ import { AllShowsContainerComponent } from './pages/all-shows-container/all-show
 		ShowCardComponent,
 		ShowListComponent,
 		AllShowsContainerComponent,
+		MainLayoutComponent,
 	],
-	imports: [BrowserModule, BrowserAnimationsModule, MatCardModule],
+	imports: [BrowserModule, BrowserAnimationsModule, MatCardModule, MatSidenavModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
