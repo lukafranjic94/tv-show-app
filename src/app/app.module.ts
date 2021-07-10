0eb2a2df153a36b4f6c832291f605e3ea22ce4de
Luka Franjić <lukafranjic94@gmail.com>
2021-07-10 18:42:18 +0200

    Add ShowCardComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RatingComponent } from './components/rating/rating.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { ShowCardComponent } from './components/show-card/show-card.component';
 
 @NgModule({
-	declarations: [AppComponent, RatingComponent, SidenavComponent],
+	declarations: [AppComponent, RatingComponent, SidenavComponent, ShowCardComponent],
 	imports: [BrowserModule, BrowserAnimationsModule, MatCardModule],
 	providers: [],
 	bootstrap: [AppComponent],
---
+++ src/app/components/show-card/show-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+
+@Component({
+	selector: 'app-show-card',
+	templateUrl: './show-card.component.html',
+	styleUrls: ['./show-card.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ShowCardComponent implements OnInit {
+	@Input() title: string;
+	@Input() averageRating: string;
+	@Input() imageUrl: string;
+
+	constructor() {}
+
+	ngOnInit(): void {}
+}
