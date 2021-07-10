e1fa30310052d235f30ae2a10a960da7ee3cfc1f
Luka Franjić <lukafranjic94@gmail.com>
2021-07-10 19:11:19 +0200

    Add AllShowsContainerComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,17 @@ import { RatingComponent } from './components/rating/rating.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { ShowCardComponent } from './components/show-card/show-card.component';
 import { ShowListComponent } from './components/show-list/show-list.component';
+import { AllShowsContainerComponent } from './pages/all-shows-container/all-shows-container.component';
 
 @NgModule({
-	declarations: [AppComponent, RatingComponent, SidenavComponent, ShowCardComponent, ShowListComponent],
+	declarations: [
+		AppComponent,
+		RatingComponent,
+		SidenavComponent,
+		ShowCardComponent,
+		ShowListComponent,
+		AllShowsContainerComponent,
+	],
 	imports: [BrowserModule, BrowserAnimationsModule, MatCardModule],
 	providers: [],
 	bootstrap: [AppComponent],
---
+++ src/app/pages/all-shows-container/all-shows-container.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Show } from 'src/app/services/show/show.model';
+
+@Component({
+	selector: 'app-all-shows-container',
+	templateUrl: './all-shows-container.component.html',
+	styleUrls: ['./all-shows-container.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AllShowsContainerComponent implements OnInit {
+	rawShows: Array<any> = [
+		{
+			title: 'Battlestar Galactica',
+			description:
+				'Battlestar Galactica (BSG) is an American military science fiction television series, and part of the Battlestar Galactica franchise. The show was developed by Ronald D. Moore and executive produced by Moore and David Eick as a re-imagining of the 1978 Battlestar Galactica television series created by Glen A. Larson.',
+			average_rating: 4.1,
+			image_url: 'https://images-na.ssl-images-amazon.com/images/I/81x1wKmDkFL._SL1500_.jpg',
+		},
+		{
+			title: 'Breaking Bad',
+			description:
+				'Breaking Bad is an American neo-Western crime drama television series created and produced by Vince Gilligan. The show aired on AMC from January 20, 2008, to September 29, 2013, consisting of five seasons for a total of 62 episodes. It was set and filmed in Albuquerque, New Mexico, and tells the story of Walter White (Bryan Cranston), an underpaid, overqualified and dispirited high school chemistry teacher who is struggling with a recent diagnosis of stage-three lung cancer.',
+			average_rating: 4.7,
+			image_url:
+				'https://m.media-amazon.com/images/M/MV5BMjhiMzgxZTctNDc1Ni00OTIxLTlhMTYtZTA3ZWFkODRkNmE2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg',
+		},
+		{
+			title: 'General',
+			description:
+				'rati životnu pripovijest hrvatskog ratnog junaka, generala Ante Gotovine. Sniman je na lokacijama u Zagrebu, Šibeniku i Livnu. Snimljen je u koprodukciji tvrtke Kiklop film i HRT-a, a podržan je od strane Hrvatskog audiovizualnog centra. Smatra ga se najočekivanijim hrvatskim filmom svih vremena.',
+			average_rating: 4.7,
+			image_url:
+				'https://www.croatiaweek.com/wp-content/uploads/2019/07/MV5BZmMxYWM3NjAtYjcyZS00ZTEzLTg4ZjctMDY3NDEwYTY0MjQzXkEyXkFqcGdeQXVyNzQwNzQwMjA@._V1_SY1000_CR006951000_AL_.jpg?x61649',
+		},
+	];
+
+	shows: Array<Show>;
+
+	constructor() {}
+
+	ngOnInit(): void {
+		this.shows = this.rawShows.map((rawShow) => new Show(rawShow));
+	}
+}
