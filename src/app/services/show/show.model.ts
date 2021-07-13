952d60fb447b821306bee47b07150ec7c101500d
Luka Franjić <lukafranjic94@gmail.com>
2021-07-13 19:48:49 +0200

    Add id field to Show model

--- src/app/services/show/show.model.ts
+++ src/app/services/show/show.model.ts
@@ -3,11 +3,13 @@ export class Show {
 	description: string;
 	averageRating: number;
 	imageUrl: string;
+	id: string;
 	constructor(rawShow: any) {
 		this.title = rawShow.title;
 		this.description = rawShow.description;
 		this.averageRating = rawShow.average_rating;
 		this.imageUrl = rawShow.image_url;
+		this.id = rawShow.id;
 	}
 	calcPercentageRating(): number {
 		const percentageRating: number = (100 * (this.averageRating - 1)) / (5 - 1);
