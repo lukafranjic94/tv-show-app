0ff89371256ff734a8b34f7db324e8f35730d6f7
Luka Franjić <lukafranjic94@gmail.com>
2021-07-13 20:14:11 +0200

    Change all shows page so that it uses show service for fetching shows

--- src/app/pages/all-shows-container/all-shows-container.component.ts
+++ src/app/pages/all-shows-container/all-shows-container.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Show } from 'src/app/services/show/show.model';
+import { ShowService } from 'src/app/services/show/show.service';
 
 @Component({
 	selector: 'app-all-shows-container',
@@ -8,41 +9,11 @@ import { Show } from 'src/app/services/show/show.model';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AllShowsContainerComponent implements OnInit {
-	rawShows: Array<any> = [
-		{
-			title: 'Battlestar Galactica',
-			description:
-				'Battlestar Galactica (BSG) is an American military science fiction television series, and part of the Battlestar Galactica franchise. The show was developed by Ronald D. Moore and executive produced by Moore and David Eick as a re-imagining of the 1978 Battlestar Galactica television series created by Glen A. Larson.',
-			average_rating: 4.1,
-			image_url: 'https://images-na.ssl-images-amazon.com/images/I/81x1wKmDkFL._SL1500_.jpg',
-		},
-		{
-			title: 'Breaking Bad',
-			description:
-				'Breaking Bad is an American neo-Western crime drama television series created and produced by Vince Gilligan. The show aired on AMC from January 20, 2008, to September 29, 2013, consisting of five seasons for a total of 62 episodes. It was set and filmed in Albuquerque, New Mexico, and tells the story of Walter White (Bryan Cranston), an underpaid, overqualified and dispirited high school chemistry teacher who is struggling with a recent diagnosis of stage-three lung cancer.',
-			average_rating: 4.7,
-			image_url:
-				'https://m.media-amazon.com/images/M/MV5BMjhiMzgxZTctNDc1Ni00OTIxLTlhMTYtZTA3ZWFkODRkNmE2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg',
-		},
-		{
-			title: 'General',
-			description:
-				'rati životnu pripovijest hrvatskog ratnog junaka, generala Ante Gotovine. Sniman je na lokacijama u Zagrebu, Šibeniku i Livnu. Snimljen je u koprodukciji tvrtke Kiklop film i HRT-a, a podržan je od strane Hrvatskog audiovizualnog centra. Smatra ga se najočekivanijim hrvatskim filmom svih vremena.',
-			average_rating: 2.5,
-			image_url:
-				'https://www.croatiaweek.com/wp-content/uploads/2019/07/MV5BZmMxYWM3NjAtYjcyZS00ZTEzLTg4ZjctMDY3NDEwYTY0MjQzXkEyXkFqcGdeQXVyNzQwNzQwMjA@._V1_SY1000_CR006951000_AL_.jpg?x61649',
-		},
-	];
+	public shows: Array<Show>;
 
-	shows: Array<Show>;
-
-	constructor() {}
+	constructor(private showService: ShowService) {}
 
 	ngOnInit(): void {
-		this.shows = this.rawShows.map((rawShow) => {
-			const newShow = new Show(rawShow);
-			console.log(`${newShow.title} ima rating ${newShow.calcPercentageRating()}%`);
-			return newShow;
-		});
+		this.shows = this.showService.getShows();
 	}
 }
