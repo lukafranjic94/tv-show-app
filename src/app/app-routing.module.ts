6f43ce8a2d9db7aefe3eba24d91458cceed61e5a
Luka Franjić <lukafranjic94@gmail.com>
2021-07-14 16:48:56 +0200

    Add top-rated and show details routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 import { AllShowsContainerComponent } from './pages/all-shows-container/all-shows-container.component';
 import { RouterModule, Routes } from '@angular/router';
+import { TopRatedContainerComponent } from './pages/top-rated-container/top-rated-container.component';
+import { ShowDetailsContainerComponent } from './pages/show-details-container/show-details-container.component';
 
 const routes: Routes = [
 	{
 		path: '',
 		component: MainLayoutComponent,
-		children: [{ path: '', component: AllShowsContainerComponent }],
+		children: [
+			{ path: '', component: AllShowsContainerComponent },
+			{ path: 'top-rated', component: TopRatedContainerComponent },
+			{ path: 'show/:id', component: ShowDetailsContainerComponent },
+		],
 	},
 	{
 		path: '**',
