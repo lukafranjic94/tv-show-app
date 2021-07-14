10b1ed7b1d77657cbdfe26e70ea3f89553f94f0d
Luka Franjić <lukafranjic94@gmail.com>
2021-07-14 17:03:20 +0200

    Change sidenav so that it renders link stored in the component's array

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ILink } from 'src/app/interfaces/link.interface';
 
 @Component({
-  selector: 'app-sidenav',
-  templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'app-sidenav',
+	templateUrl: './sidenav.component.html',
+	styleUrls: ['./sidenav.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SidenavComponent implements OnInit {
+	public links: Array<ILink> = [
+		{
+			title: 'All shows',
+			url: '',
+		},
+		{
+			title: 'Top rated',
+			url: '/top-rated',
+		},
+	];
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
