5e5b6e90fe38468be05b562000f345c8f06cea39
Luka Franjić <lukafranjic94@gmail.com>
2021-07-17 17:43:08 +0200

    Add 'reviews' field to Show model and update averageRating method

--- src/app/services/show/show.model.ts
+++ src/app/services/show/show.model.ts
@@ -1,17 +1,25 @@
 import { IRawShow } from 'src/app/interfaces/rawShow.interface';
+import { Review } from '../review/review.model';
 
 export class Show {
 	public title: string;
 	public description: string;
-	public averageRating: number;
 	public imageUrl: string;
 	public id: string;
+	public reviews: Array<Review>;
 	constructor(rawShow: IRawShow) {
 		this.title = rawShow.title;
 		this.description = rawShow.description;
-		this.averageRating = rawShow.average_rating;
 		this.imageUrl = rawShow.image_url;
 		this.id = rawShow.id;
+		this.reviews = [];
+	}
+	public get averageRating(): number {
+		if (this.reviews.length === 0) {
+			return 0;
+		}
+		let average: number = this.reviews.map((review) => review.rating).reduce((a, b) => a + b) / this.reviews.length;
+		return average;
 	}
 	public calcPercentageRating(): number {
 		const percentageRating: number = (100 * (this.averageRating - 1)) / (5 - 1);
