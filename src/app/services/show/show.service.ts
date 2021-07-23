1d7edc0cb6a5d4e5ee846e4e0b019db49d7b926a
Luka Franjić <lukafranjic94@gmail.com>
2021-07-23 18:14:58 +0200

    Fetch shows from backend

--- src/app/services/show/show.service.ts
+++ src/app/services/show/show.service.ts
@@ -1,8 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { combineLatest, Observable, of, throwError } from 'rxjs';
-import { delay, map, switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { IRawShow } from 'src/app/interfaces/rawShow.interface';
-import { Review } from '../review/review.model';
 import { ReviewService } from '../review/review.service';
 import { Show } from './show.model';
 
@@ -10,59 +10,23 @@ import { Show } from './show.model';
 	providedIn: 'root',
 })
 export class ShowService {
-	private rawShows: Array<IRawShow> = [
-		{
-			title: 'Battlestar Galactica',
-			description:
-				'Battlestar Galactica (BSG) is an American military science fiction television series, and part of the Battlestar Galactica franchise. The show was developed by Ronald D. Moore and executive produced by Moore and David Eick as a re-imagining of the 1978 Battlestar Galactica television series created by Glen A. Larson.',
-			average_rating: 4.1,
-			image_url: 'https://images-na.ssl-images-amazon.com/images/I/81x1wKmDkFL._SL1500_.jpg',
-			id: '1',
-		},
-		{
-			title: 'Breaking Bad',
-			description:
-				'Breaking Bad is an American neo-Western crime drama television series created and produced by Vince Gilligan. The show aired on AMC from January 20, 2008, to September 29, 2013, consisting of five seasons for a total of 62 episodes. It was set and filmed in Albuquerque, New Mexico, and tells the story of Walter White (Bryan Cranston), an underpaid, overqualified and dispirited high school chemistry teacher who is struggling with a recent diagnosis of stage-three lung cancer.',
-			average_rating: 4.7,
-			image_url:
-				'https://m.media-amazon.com/images/M/MV5BMjhiMzgxZTctNDc1Ni00OTIxLTlhMTYtZTA3ZWFkODRkNmE2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg',
-			id: '2',
-		},
-		{
-			title: 'General',
-			description:
-				'rati životnu pripovijest hrvatskog ratnog junaka, generala Ante Gotovine. Sniman je na lokacijama u Zagrebu, Šibeniku i Livnu. Snimljen je u koprodukciji tvrtke Kiklop film i HRT-a, a podržan je od strane Hrvatskog audiovizualnog centra. Smatra ga se najočekivanijim hrvatskim filmom svih vremena.',
-			average_rating: 2.5,
-			image_url:
-				'https://www.croatiaweek.com/wp-content/uploads/2019/07/MV5BZmMxYWM3NjAtYjcyZS00ZTEzLTg4ZjctMDY3NDEwYTY0MjQzXkEyXkFqcGdeQXVyNzQwNzQwMjA@._V1_SY1000_CR006951000_AL_.jpg?x61649',
-			id: '3',
-		},
-	];
-
-	constructor(private reviewService: ReviewService) {}
-
-	private get shows(): Array<Show> {
-		return this.rawShows.map((rawShow: IRawShow) => new Show(rawShow));
-	}
+	constructor(private reviewService: ReviewService, private http: HttpClient) {}
 
 	public getShows(): Observable<Array<Show>> {
-		return of(this.shows).pipe(
-			delay(1000 + Math.random() * 1000),
-			map((shows: Array<Show>) => {
-				let rndNum: number = Math.random();
-				if (rndNum >= 0.9) {
-					throw new Error('error message');
-				}
-				return shows;
-			})
-		);
+		return this.http
+			.get<{ shows: Array<IRawShow> }>('https://tv-shows.infinum.academy/shows')
+			.pipe(map((response) => response.shows.map((rawShow) => new Show(rawShow))));
 	}
 
 	public getTopRated(): Observable<Array<Show>> {
-		return this.getShows().pipe(map((shows: Array<Show>) => shows.filter((show: Show) => show.averageRating > 4)));
+		return this.http
+			.get<{ shows: Array<IRawShow> }>('https://tv-shows.infinum.academy/shows/top_rated')
+			.pipe(map((response) => response.shows.map((rawShow) => new Show(rawShow))));
 	}
 
 	public getShow(id: string): Observable<Show | undefined> {
-		return this.getShows().pipe(map((shows: Array<Show>) => shows.find((show: Show) => show.id === id)));
+		return this.http
+			.get<{ show: IRawShow }>(`https://tv-shows.infinum.academy/shows/${id}`)
+			.pipe(map((response) => new Show(response.show)));
 	}
 }
