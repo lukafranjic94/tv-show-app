6b2728d48081a3885fc8a6921f0c5995ff07aa61
Luka Franjić <lukafranjic94@gmail.com>
2021-07-31 17:22:58 +0200

    Add validation to user profile image upload

--- src/app/pages/user-profile-container/user-profile-container.component.ts
+++ src/app/pages/user-profile-container/user-profile-container.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { BehaviorSubject, merge, Observable, Subject } from 'rxjs';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
-import { AuthService } from 'src/app/services/auth/auth.service';
 import { User } from 'src/app/services/user/user.model';
 import { UserService } from 'src/app/services/user/user.service';
 
@@ -18,33 +17,45 @@ export class UserProfileContainerComponent {
 			return this.userService.getUser();
 		})
 	);
-	constructor(private authService: AuthService, private userService: UserService) {}
+	constructor(private userService: UserService) {}
 
-	public drop(event: any): void {
-		event.preventDefault();
-		event.stopPropagation();
-		this.handleFiles(event.dataTransfer.files);
+	public drop(event: DragEvent): void {
+		this.preventDefaults(event);
+		if (event.dataTransfer) {
+			this.handleFiles(event.dataTransfer.files);
+		}
 	}
 
-	public dragOver(event: any): void {
-		event.preventDefault();
-		event.stopPropagation();
+	public dragOver(event: DragEvent): void {
+		this.preventDefaults(event);
 	}
 
-	public dragLeave(event: any): void {
-		event.preventDefault();
+	public dragEnter(event: DragEvent): void {
+		this.preventDefaults(event);
+	}
+
+	private preventDefaults(event: Event): void {
 		event.stopPropagation();
+		event.preventDefault();
 	}
 
 	public handleFiles(files: FileList | null): void {
-		if (files && files.length === 1) {
-			const fileArr: Array<File> = Array.from(files);
-			const file: File = fileArr[0];
-			const formData: FormData = new FormData();
-			formData.append('image', file);
-			this.userService.addProfileImage(formData).subscribe(() => {
-				this.fetchTrigger$.next(undefined);
-			});
+		if (!files || files.length !== 1) {
+			return;
 		}
+
+		const fileArr: Array<File> = Array.from(files);
+		const file: File = fileArr[0];
+		const imageRegExp: RegExp = new RegExp('^image*');
+
+		if (!imageRegExp.test(file.type)) {
+			return;
+		}
+
+		const formData: FormData = new FormData();
+		formData.append('image', file);
+		this.userService.addProfileImage(formData).subscribe(() => {
+			this.fetchTrigger$.next(undefined);
+		});
 	}
 }
